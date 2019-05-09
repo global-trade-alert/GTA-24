c852ed7ec90d1aed183d4c43a18ac852c1e9ce0a
Johannes <fritz.johannes@gmail.com>
2019-05-09 10:08:46 +0200

    added waterfall charts.
    plus some edits

--- code/ppt/SE at ISC.R
+++ code/ppt/SE at ISC.R
@@ -20,7 +20,7 @@ loadfonts(device="postscript")
 loadfonts(device="win")
 
 
-#setwd(""C:/Users/jfrit/Desktop/Dropbox/GTA cloud")
+setwd("C:/Users/jfrit/Desktop/Dropbox/GTA cloud")
 setwd("C:/Users/Piotr Lukaszuk/Dropbox/GTA cloud")
 #setwd('C:/Users/Kamran/Dropbox/GTA cloud')
 # setwd('D:/Dropbox/Dropbox/GTA cloud')
@@ -29,6 +29,7 @@ output.path="0 report production/GTA 24/tables & figures/ppt"
 # Prepare simplified versions of figures 3.3 and 3.4. In one version please delete the detail of the number of interventions affecting trade. 
 #   Please also prepare a version of 3.4 (but not for 3.3) where the number of hits detail is retained.
 
+source("0 report production/GTA 24/help files/GTA 24 cutoff and definitions.R")
 
 for(fig in c(3.3, 3.4)){
   
@@ -42,21 +43,28 @@ for(fig in c(3.3, 3.4)){
     aj.guy="Chinese"
     ij="US"
     ij.guy="US"
+    y.max=500
   } else {
     ij="China"
     ij.guy="Chinese"
     aj="US"
     aj.guy="US"
     
+    y.max=150
+    
   }
+
   
   ## totals
-  y.max=round(max(fig3.3.plot$value)/100000000000 +.5,0)*100
+  # y.max=round(max(fig3.3.plot$value)/100000000000 +.5,0)*100
+  
   just.total <- ggplot()+
     geom_bar(data=subset(fig3.3.plot, variable == "value.total"), 
              aes(x=administration, y=value/1000000000), 
              stat = "identity", width=0.6,
              fill=gta_colour$blue[1]) +
+    geom_line(data=subset(fig3.3.plot, variable == "share.total"), aes(x=administration, y=value*y.max, group=1),colour=gta_colour$qualitative[6],size=1.5)+
+    geom_text(data=subset(fig3.3.plot, variable == "share.total"), aes(x=administration, y=value*y.max, label=round(value, digits = 3)), nudge_y = -20, size=3.5, colour="#FFFFFF")+
     scale_y_continuous(breaks=seq(0,y.max,50), limits = c(0,y.max),sec.axis = dup_axis())+
     scale_x_discrete(labels = c("Obama II", "2017", "2018", "2019"))+
     xlab("Period")+
@@ -74,28 +82,28 @@ for(fig in c(3.3, 3.4)){
                  name=paste("Figure ",fig," - Total value of ",aj.guy," exports affected by ",ij.guy, sep=""),
                  eps=F)
   
-  ## by hit bracket, no trade share
-  just.hits <- ggplot(data=subset(fig3.3.plot, variable %in% c("value.1","value.2","value.3","value.4","value.5")), 
-                      aes(x=administration, y=value/1000000000, fill=variable))+
-    geom_bar(stat = "identity", width=0.6) +
-    scale_y_continuous(breaks=seq(0,y.max,50), limits = c(0,y.max),sec.axis = dup_axis())+
-    scale_x_discrete(labels = c("Obama II", "2017", "2018", "2019"))+
-    scale_fill_manual(values = gta_colour$qualitative[c(5,4,3,2,1)], labels=c("1","2","3","4","5 or more"))+
-    xlab("Period")+
-    ylab(paste(aj.guy," exports affected\n(billion US Dollars)",sep=""))+
-    guides(fill=guide_legend(title="Number of interventions \naffecting trade", ncol=3,title.position = "top"))+
-    gta_theme()+
-    theme(axis.text.x.bottom = element_text(size=12),
-          axis.title.y.left = element_text(size=12),
-          axis.title.y.right = element_text(size=12)
-    )
-  
-  just.hits
-  
-  gta_plot_saver(plot=just.hits,
-                 path=output.path,
-                 name=paste("Figure ",fig," - Value of ",aj.guy," exports affected by ",ij.guy," x or more times", sep=""),
-                 eps=F)
+  # ## by hit bracket, no trade share
+  # just.hits <- ggplot(data=subset(fig3.3.plot, variable %in% c("value.1","value.2","value.3","value.4","value.5")), 
+  #                     aes(x=administration, y=value/1000000000, fill=variable))+
+  #   geom_bar(stat = "identity", width=0.6) +
+  #   scale_y_continuous(breaks=seq(0,y.max,50), limits = c(0,y.max),sec.axis = dup_axis())+
+  #   scale_x_discrete(labels = c("Obama II", "2017", "2018", "2019"))+
+  #   scale_fill_manual(values = gta_colour$qualitative[c(5,4,3,2,1)], labels=c("1","2","3","4","5 or more"))+
+  #   xlab("Period")+
+  #   ylab(paste(aj.guy," exports affected\n(billion US Dollars)",sep=""))+
+  #   guides(fill=guide_legend(title="Number of interventions \naffecting trade", ncol=3,title.position = "top"))+
+  #   gta_theme()+
+  #   theme(axis.text.x.bottom = element_text(size=12),
+  #         axis.title.y.left = element_text(size=12),
+  #         axis.title.y.right = element_text(size=12)
+  #   )
+  # 
+  # just.hits
+  # 
+  # gta_plot_saver(plot=just.hits,
+  #                path=output.path,
+  #                name=paste("Figure ",fig," - Value of ",aj.guy," exports affected by ",ij.guy," x or more times", sep=""),
+  #                eps=F)
   
   
 }
@@ -148,13 +156,13 @@ tc.plot <- gather(trade.coverage, year, share, c(2:ncol(trade.coverage)))
 tc.plot$year=as.numeric(tc.plot$year)
 tc.plot=rbind(tc.plot,
               data.frame(exporter=c("Botswana","Singapore"),
-                         year=2008.9,
+                         year=2008,
                          share=0))
 
 tc.2.cty= ggplot(tc.plot, aes(x=year, y=share, color=as.factor(exporter)))+
   geom_line(size=2)+
   scale_color_manual(values=c(gta_colour$qualitative[c(1,7)]))+
-  scale_x_continuous(limit=c(2008.5,2019),breaks=seq(2009,2019,1))+
+  scale_x_continuous(limit=c(2008,2019),breaks=seq(2008,2019,1), labels = c("Nov.\n2008",2009:2019))+
   scale_y_continuous(limit=c(0,1),breaks=seq(0,1,.2), sec.axis = dup_axis())+
   labs(x="", y="Share of total exports harmed", color="")+
   guides(fill=element_blank())+
@@ -187,13 +195,13 @@ tcw.plot <- gather(trade.coverage.world, year, share, c(2:ncol(trade.coverage)))
 tcw.plot$year=as.numeric(tcw.plot$year)
 tcw.plot=rbind(tcw.plot,
               data.frame(exporter=c("World"),
-                         year=2008.9,
+                         year=2008,
                          share=0))
 
 tc.2.cty= ggplot(tcw.plot, aes(x=year, y=share, color=as.factor(exporter)))+
   geom_line(size=2)+
   scale_color_manual(values=c(gta_colour$red[2]))+
-  scale_x_continuous(limit=c(2008.5,2019),breaks=seq(2009,2019,1))+
+  scale_x_continuous(limit=c(2008,2019),breaks=seq(2008,2019,1), labels = c("Nov.\n2008",2009:2019))+
   scale_y_continuous(limit=c(0,1),breaks=seq(0,1,.2), sec.axis = dup_axis())+
   labs(x="", y="Share of total exports harmed", color="")+
 guides(fill=element_blank())+
@@ -220,7 +228,7 @@ total.plot=rbind(tc.plot, tcw.plot)
 tc.2.cty= ggplot(total.plot, aes(x=year, y=share, color=as.factor(exporter)))+
   geom_line(size=2)+
   scale_color_manual(values=c(gta_colour$qualitative[c(1,7)],gta_colour$red[2]))+
-  scale_x_continuous(limit=c(2008.5,2019),breaks=seq(2009,2019,1))+
+  scale_x_continuous(limit=c(2008,2019),breaks=seq(2008,2019,1), labels = c("Nov.\n2008",2009:2019))+
   scale_y_continuous(limit=c(0,1),breaks=seq(0,1,.2), sec.axis = dup_axis())+
   labs(x="", y="Share of total exports harmed", color="")+
 guides(fill=element_blank())+
@@ -272,25 +280,47 @@ gta_trade_coverage(coverage.period=c(2018,2018),
                    intra.year.duration = F,
                    nr.exporters = c(2,999))
 
+other.stuff=sum(trade.coverage.estimates[,4])
 
-war.perspective=data.frame(act.title="Other\nUS/CHN\ntariff\nincreases\nin 2018",
-                           value=sum(trade.coverage.estimates[,4]))
-
-
-# third column from the left would add Trump’s very recent tariff threats on top. 
-tweet.war=63051
 gta_trade_coverage(coverage.period=c(2018,2018),
-                   intervention.ids = tweet.war,
-                   keep.interventions = T,
-                   exporters = "China",
+                   gta.evaluation = c("Red", "Amber"),
+                   affected.flows = "inward",
+                   implementers=c("China","United States of America"),
+                   keep.implementer = T,
+                   exporters = c("China","United States of America"),
                    keep.exporters = T,
+                   intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
+                   keep.type = F,
+                   implementation.period = c("2018-01-01","2018-12-31"),
                    trade.data="2017",
                    trade.statistic = "value",
-                   intra.year.duration = F)
+                   intra.year.duration = F,
+                   nr.exporters = c(2,999))
 
-war.perspective=rbind(war.perspective,
-                      data.frame(act.title="May\n2019\nthreat",
-                                 value=trade.coverage.estimates[,4]))
+other.stuff=other.stuff+sum(trade.coverage.estimates[,4])
+
+gta_trade_coverage(coverage.period=c(2018,2018),
+                   gta.evaluation = c("Red", "Amber"),
+                   affected.flows = "inward",
+                   implementers=c("China","United States of America"),
+                   keep.implementer = T,
+                   exporters = c("China","United States of America"),
+                   keep.exporters = T,
+                   intervention.types = unique(intervention.groups$intervention.type[intervention.groups$group.name=="Tariffs and trade defence"]),
+                   keep.type = F,
+                   implementation.period = c("2018-01-01","2018-12-31"),
+                   trade.data="2017",
+                   trade.statistic = "value",
+                   intra.year.duration = F,
+                   nr.exporters = c(1,1))
+
+other.stuff=other.stuff+sum(trade.coverage.estimates[,4])
+
+
+
+war.perspective=data.frame(act.title="Other\nUS/CHN\ninterventions\nin 2018",
+                           value=other.stuff)
+
 
 # second the next column on the left would add the Chinese tariff increases and 
 gta_trade_coverage(coverage.period=c(2018,2018),
@@ -303,7 +333,7 @@ gta_trade_coverage(coverage.period=c(2018,2018),
                    intra.year.duration = F)
 
 war.perspective=rbind(war.perspective,
-                      data.frame(act.title="Chinese\n2018\ntariffs",
+                      data.frame(act.title="China\ntariffs\n2018",
                                  value=trade.coverage.estimates[,4]))
 
 # Then the first left hand column would have the Trump tariff increases of 2018, 
@@ -317,8 +347,24 @@ gta_trade_coverage(coverage.period=c(2018,2018),
                    intra.year.duration = F)
 
 war.perspective=rbind(war.perspective,
-                      data.frame(act.title="US\n2018\ntariffs",
+                      data.frame(act.title="US\ntariffs\n2018",
                                  value=trade.coverage.estimates[,4]))
+
+# third column from the left would add Trump’s very recent tariff threats on top. 
+# tweet.war=63051
+# gta_trade_coverage(coverage.period=c(2018,2018),
+#                    intervention.ids = tweet.war,
+#                    keep.interventions = T,
+#                    exporters = "China",
+#                    keep.exporters = T,
+#                    trade.data="2017",
+#                    trade.statistic = "value",
+#                    intra.year.duration = F)
+
+war.perspective=rbind(war.perspective,
+                      data.frame(act.title="Trump\nthreat\nMay\n2019",
+                                 value=us.chn-trade.coverage.estimates[,4]))
+
 
 
 ## total bilateral trade
@@ -327,8 +373,49 @@ war.perspective=rbind(war.perspective,
                            value=chn.us+us.chn))
 
 
+xlsx::write.xlsx(war.perspective, file=paste(output.path, "/Trade war interventions in US-CHN bilateral perspective.xlsx", row.names=F))
 
 
+## exciting waterfall version
+## based on https://learnr.wordpress.com/2010/05/10/ggplot2-waterfall-charts/
+war.terfall=war.perspective
+war.terfall$value=round(war.terfall$value/1000000000,2)
+war.terfall$act.title=factor(war.terfall$act.title, levels=war.terfall$act.title)
+war.terfall$id=1:nrow(war.terfall)
+war.terfall$end=cumsum(war.terfall$value)
+war.terfall$end[nrow(war.terfall)]=war.terfall$value[nrow(war.terfall)]
+war.terfall$end[nrow(war.terfall)-1]=war.terfall$value[nrow(war.terfall)]
+war.terfall$value[nrow(war.terfall)]=war.terfall$end[nrow(war.terfall)]-war.terfall$end[nrow(war.terfall)-1]
+war.terfall$start <- c(0, head(war.terfall$end, -1))
+
+war.terfall$start[nrow(war.terfall)-1]=war.terfall$end[nrow(war.terfall)]-war.terfall$value[nrow(war.terfall)-1]
+war.terfall$start[nrow(war.terfall)]=0
+war.terfall=war.terfall[,c(3,1,5,4,2)]
+
+warter.shed=ggplot(war.terfall, aes(act.title, fill = act.title)) + 
+  geom_rect(aes(x = act.title, 
+                xmin = id - 0.45, xmax = id + 0.45, 
+                ymin = end, ymax = start))+
+  scale_fill_manual(values=c(gta_colour$qualitative[c(1,7,5,3,8)]))+
+  scale_y_continuous(limit=c(0,700),breaks=seq(0,700,100), sec.axis = dup_axis())+
+  labs(x="", y="USD billion", color="")+
+  gta_theme(x.bottom.angle = 0)+
+  theme(axis.text.x.bottom = element_text(vjust = 0.5, size=18),
+        axis.text.y.left = element_text(size=18),
+        axis.text.y.right = element_text(size=18),
+        axis.title.y.left = element_text(size=16),
+        axis.title.y.right = element_text(size=16),
+        axis.title.x.bottom = element_text(size=18),
+        legend.position="none")
+
+gta_plot_saver(plot=warter.shed,
+               path=output.path,
+               name="Trade war interventions in US-CHN bilateral perspective - waterfall chart",
+               eps=F)
+
+
+
+## boring bar plot version
 war.picture=ggplot(war.perspective, aes(x = factor(act.title), y = round(value/1000000000,0), fill=act.title)) +
   geom_col() + 
   geom_text(aes(y = round(value/1000000000,0) + 30, label = round(value/1000000000,0)), size=5,position = position_dodge(w = -0.5))+
